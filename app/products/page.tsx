24f8a5fb03cf2d25232dbb7b1960205e633ccc68
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-05-11 17:59:03 +0530

    Dynamic routes completed

---
+++ app/products/page.tsx
@@ -0,0 +1,12 @@
+export default function ProductsPage() {
+    return (
+        <div>
+            <h1>Products Page</h1>
+            <p>Welcome to the products page. Here you can find all our amazing products!</p>
+
+            <h2>Product 1</h2>
+            <h2>Product 2</h2>
+            <h2>Product 3</h2>
+        </div>
+    )
+}
