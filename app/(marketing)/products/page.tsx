6a0fff8c4c7c275d8ebf6eae2d9d4bf899d6a329
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-05-12 14:13:15 +0530

    Link component finished

--- app/(marketing)/products/page.tsx
+++ app/(marketing)/products/page.tsx
@@ -1,4 +1,7 @@
+import Link from "next/link"
+
 export default function ProductsPage() {
+    const productId = 100;
     return (
         <div>
             <h1>Products Page</h1>
@@ -6,7 +9,16 @@ export default function ProductsPage() {
 
             <h2>Product 1</h2>
             <h2>Product 2</h2>
-            <h2>Product 3</h2>
+            <h2>
+                <Link href={`/products/3`} replace> 
+                    Product 3
+                </Link>
+            </h2>
+            <h2>
+                <Link href={`/products/${productId}`}>
+                    Product 4
+                </Link>
+            </h2>
         </div>
     )
 }
