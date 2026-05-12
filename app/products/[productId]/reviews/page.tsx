822b0e70065ebc094e79c78b4fa05fe0f0656ed5
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-05-12 11:01:16 +0530

    initial commit

--- app/products/[productId]/reviews/page.tsx
+++ app/products/[productId]/reviews/page.tsx
@@ -7,10 +7,11 @@ export default async function ReviewDetails({
     return (
         <>
             <h1>Reviews for Product {productId}</h1>
-            
+
             <li>Review 1</li>
             <li>Review 2</li>
             <li>Review 3</li>
+            <li>Review 4</li>
         </>
     )
 }
