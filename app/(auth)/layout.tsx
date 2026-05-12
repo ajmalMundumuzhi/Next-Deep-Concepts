7b172110131b33e22dc60ba4320b6f0e463ba6ff
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-05-12 14:25:36 +0530

    Active Link Completed

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,36 @@
+"use client";
+
+import Link from "next/link"
 import "../globals.css"
+import { usePathname } from "next/navigation";
+
+const navLinks = [
+  { name: "Register ", href: "/register" },
+  { name: "Login ", href: "/login" },
+  { name: "Forgot Password", href: "/forgot-password" },
+]
 
 export default function AuthLayout({
   children,
 } : {
   children: React.ReactNode
 }) {
+  const pathname = usePathname();
   return (
     <html lang="eng">
       <body>
+        <nav>
+          { navLinks.map((link) => {
+            const isActive = pathname === link.href || (pathname.startsWith(link.href) && link.href !== "/");
+            return (
+              <Link className={isActive ? "text-blue-500": ""} 
+              href={link.href} 
+              key={link.name}>
+                {link.name}
+              </Link>
+            )
+          })}
+        </nav>
       {children}
       <footer>
           <div className="bg-gray-800 text-white p-4 text-center">
