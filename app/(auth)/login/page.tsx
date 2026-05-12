0a22ffc008e2fcbfbf774f8ff13c033479df7e43
ajmalMundumuzhi <muhajmal73@gmail.com>
2026-05-12 12:09:01 +0530

    route groups completed

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+export default function ForgotPassword() {
+    return (
+        <h1>
+            Login Page.
+        </h1>
+    )
+}
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,7 @@
+export default function ForgotPassword() {
+    return (
+        <h1>
+            Register Page.
+        </h1>
+    )
+}
